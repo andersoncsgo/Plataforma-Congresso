dd1f439af67b9b0a805afc7887db18327687c6bd
Giovana Serpa <gel_serpa@hotmail.com>
2026-01-03 15:37:35 -0300

    Create CadastroForm for user registration

---
+++ backend/plataforma/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from django.contrib.auth.models import User
+
+class CadastroForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ["first_name", "email", "username", "password"]
+
+    def clean(self):
+        cleaned_data = super().clean()
+        if cleaned_data.get("password") != cleaned_data.get("confirm_password"):
+            self.add_error("confirm_password", "As senhas não coincidem")
